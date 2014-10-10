fca53b0353aac521855015076b0b568a9cb62dc5
Marek Hawrylczak <wladyslaw.hawrylczak@allegro.pl>
2014-10-10 09:12:04 +0200

    step-02 challenge - async search method

--- src/main/java/pl/allegro/atm/workshop/rx/OfferResource.java
+++ src/main/java/pl/allegro/atm/workshop/rx/OfferResource.java
@@ -1,9 +1,13 @@
 package pl.allegro.atm.workshop.rx;
 
 import pl.allegro.atm.workshop.rx.mobius.MobiusClient;
+import pl.allegro.atm.workshop.rx.mobius.model.DoGetItemsListCollection;
+import rx.Observable;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 import java.util.List;
 
 @Path("/offers")
@@ -15,10 +19,17 @@ public class OfferResource {
     @Inject
     private MobiusClient mobiusClient;
 
+//TODO
+//was:
+//    @GET
+//    public List<Offer> search(@QueryParam("q") String searchString) {
+//
     @GET
-    public List<Offer> search(@QueryParam("q") String searchString) {
-        return offerAssembler.convert(
-                mobiusClient.searchOffers(searchString).toBlocking().single());
+    public void search(@QueryParam("q") String searchString, @Suspended final AsyncResponse asyncResponse) {
+        Observable<DoGetItemsListCollection> observableOffers =  mobiusClient.searchOffers(searchString);
+//now use:
+//        asyncResponse.resume(List<Offer>) and asyncResponse.resume(Throwable)
+//        offerAssembler.convert
     }
 
     @GET
--- src/main/java/pl/allegro/atm/workshop/rx/Server.java
+++ src/main/java/pl/allegro/atm/workshop/rx/Server.java
@@ -4,7 +4,7 @@ import pl.allegro.tech.bootstrap.core.infrastructure.RestService;
 
 public class Server {
     public static final void main(String[] args) {
-        RestService.main(new String[]{});
+        RestService.main(new String[]{"-async"});
     }
 }
 
